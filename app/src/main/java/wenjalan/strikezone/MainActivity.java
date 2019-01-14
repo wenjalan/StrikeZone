766bee4dd6260550d51ed15fe3e058e74be6a699
Alan Wen <alanjameswen@outlook.com>
2019-01-14 08:30:54 -0800

    Reinit
    Reinitialized project with source from HelloOpenCV

---
+++ app/src/main/java/wenjalan/strikezone/MainActivity.java
@@ -0,0 +1,99 @@
+package wenjalan.strikezone;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "HelloOpenCV";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV initialized successfully");
+        }
+        else {
+            Log.d(TAG, "OpenCV failed to initialize");
+        }
+    }
+
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS:
+//                {
+//                    Log.i(TAG, "OpenCV loaded successfully");
+//                    mOpenCvCameraView.enableView();
+//                } break;
+//                default:
+//                {
+//                    Log.d(TAG, "OpenCV manager not found");
+//                    super.onManagerConnected(status);
+//                } break;
+//            }
+//        }
+//    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        // OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_4_1_0, this, mLoaderCallback);
+        OpenCVLoader.initDebug();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.CameraView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        // set the size
+        // mOpenCvCameraView.set
+
+        // turn on the camera
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+}
