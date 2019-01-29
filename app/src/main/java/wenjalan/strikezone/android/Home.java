0e47e3edfb96bcc439719d04764df385f49453c1
Alan Wen <alanjameswen@outlook.com>
2019-01-29 08:46:47 -0800

    Strike Zone Box
    The Strike Zone Box now appears on screen, satisfying once again our first success criterion

--- app/src/main/java/wenjalan/strikezone/android/Home.java
+++ app/src/main/java/wenjalan/strikezone/android/Home.java
@@ -2,6 +2,9 @@ package wenjalan.strikezone.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
 import wenjalan.strikezone.R;
@@ -38,10 +41,44 @@ public class Home extends AppCompatActivity {
     // initialization
     protected void init() {
         // find the viewfinder
-        FrameLayout viewfinder = findViewById(R.id.viewfinder);
+        FrameLayout viewfinder = findViewById(R.id.ViewFinder);
 
         // initialize CameraManager
         this.cameraManager = new CameraManager(this, viewfinder);
+
+        // initialize the strike zone box
+        initStrikeZoneBox();
+    }
+
+    // initializes the size of the strike zone based on the height of the screen
+    // TODO: Write about this in my docs
+    protected void initStrikeZoneBox() {
+        // find the view to resize
+        final View strikeZoneBox = findViewById(R.id.StrikeZoneBox);
+
+        // once the initial layout has been completed
+        strikeZoneBox.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // get the size of the constraint layout this view resides in
+                View container = (View) strikeZoneBox.getParent();
+                int containerWidth = container.getWidth();
+                int containerHeight = container.getHeight();
+
+                // set the width and height of the box according to the ratio specified
+                ViewGroup.LayoutParams params = strikeZoneBox.getLayoutParams();
+                params.height = containerHeight / 2;
+                params.width = containerWidth / 2;
+
+                // request layout
+                strikeZoneBox.requestLayout();
+            }
+        });
+
+        // find the height of the screen
+
+        // request layout
+        strikeZoneBox.requestLayout();
     }
 
 }
